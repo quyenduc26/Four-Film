a101688fd93c945d76b6c3731434468bd17c8e0c
quyenduc26 <quyenduc26@gmail.com>
2023-10-16 02:07:16 +0700

    update new features

--- js/Feature_seat_quyen.js
+++ js/Feature_seat_quyen.js
@@ -1,27 +1,44 @@
 //CHECK AVAILABLE SEAT THEN SELECT
 document.addEventListener("DOMContentLoaded", ()=> {
     var seats = document.querySelectorAll(".seat");                                                 // Lọc tất cả các ghế
+    var seatInstance = '';
+    var seatCost = 0;
+    var seatPrice = '';
     seats.forEach((seat) =>{ 
         var isUnavailable = seat.getAttribute('data-available') === 'false';                          // Kiểm tra xem ghế còn không
         if(isUnavailable){                                                                 
             seat.removeAttribute("data-selected");                                                    // Nếu kh còn thì xoá data-selected
-            // seat.innerHTML = ''
         }
-        else{
-            seats.forEach((seat) => {                                                                 // Cho chọn ghế còn lại 
+        else{                                                           
             seat.addEventListener("click",()=> {
+                var seatPrice = parseInt(seat.getAttribute("data-price"))
                 var isSelected = seat.getAttribute("data-selected") === "true";
                 if (isSelected) {
                 seat.setAttribute("data-selected", "false");
-                //   seat.classList.remove("selected");
+                seatInstance = seatInstance.replace(seat.textContent+' ',' ');
+                seatCost -= seatPrice;
+
                 } else {
                 seat.setAttribute("data-selected", "true");
-                //   seat.classList.add("selected");
+                seatInstance += seat.textContent+' ';
+                seatCost += seatPrice;
+
                 }
+                document.getElementById('seat').innerHTML = seatInstance;
+                var printCost = seatCost.toLocaleString('vi-VN',{
+                    style: 'currency',
+                    currency: 'VND'
+                });
+                document.getElementById('total_seat').innerHTML = printCost;
+                console.log(seatInstance)
+                console.log(seatCost)
+
             });
-        });
         }
-    })
+    });
 })
 
+
+
+  
   
